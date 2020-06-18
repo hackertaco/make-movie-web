465115b21d06455ec5521097e311b2b9917aa1a1
hackertaco <imsa1224@naver.com>
2020-06-18 15:15:52 +0900

    chore:📦basic setting for website

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,8 @@
-import React from 'react'
+import React from "react";
 import { FaCode } from "react-icons/fa";
 
 function LandingPage() {
-    return (
-        <>
-        <div className="app">
-            <FaCode style={{ fontSize: '4rem' }} /><br />
-            <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-        </div>
-        <div style={{ float:'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  return <></>;
 }
 
-export default LandingPage
+export default LandingPage;
