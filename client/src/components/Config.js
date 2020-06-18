1e8fc5654f4d608e61d3d7ffe6386a89b0e3dd0c
hackertaco <imsa1224@naver.com>
2020-06-18 16:02:11 +0900

    chore: 📦add api_key

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -3,3 +3,4 @@ export const USER_SERVER = "/api/users";
 
 export const API_URL = "https://api.themoviedb.org/3/";
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+export const API_KEY = "ffe5a9f5a967b7c0827a070192030153";
