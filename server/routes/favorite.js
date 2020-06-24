c324c44828f7833c16328d1622590f1e422b2fbf
hackertaco <imsa1224@naver.com>
2020-06-24 18:52:54 +0900

    feat: ⚡server/add and remove

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -31,5 +31,22 @@ router.post("/favorited", (req, res) => {
   });
   //그 다음에 프론트에 숫자 정보 보내주기
 });
+router.post("/removeFromFavorite", (req, res) => {
+  Favorite.findOneAndDelete({
+    movieId: req.body.movieId,
+    userFrom: req.body.userFrom,
+  }).exec((err, doc) => {
+    if (err) return res.status(400).send(err);
+    return res.status(200).json({ success: true, doc });
+  });
+});
+router.post("/addToFavorite", (req, res) => {
+  //내가 이 영화를 favorite 리스트에 넣었는지 정보를 db에서 가져오기
+  const favorite = new Favorite(req.body);
+  favorite.save((err, doc) => {
+    if (err) return res.status(400).send(err);
+    return res.status(200).json({ success: true });
+  });
+});
 
 module.exports = router;
