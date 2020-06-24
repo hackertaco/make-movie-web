b60bc8664a00bd9606fcb498302bd1889683b6e6
hackertaco <imsa1224@naver.com>
2020-06-24 18:00:24 +0900

    feat: ⚡server: connect userFrom

--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -12,4 +12,24 @@ router.post("/favoriteNumber", (req, res) => {
   });
   //그 다음에 프론트에 숫자 정보 보내주기
 });
+
+router.post("/favorited", (req, res) => {
+  //내가 이 영화를 favorite 리스트에 넣었는지 정보를 db에서 가져오기
+  req.body.movieId;
+
+  //mongoDB에서 FAVORITE 숫자를 가져오기
+  Favorite.find({
+    movieId: req.body.movieId,
+    userFrom: req.body.userFrom,
+  }).exec((err, info) => {
+    if (err) return res.status(400).send(err);
+    let result = false;
+    if (info.length !== 0) {
+      result = true;
+    }
+    res.status(200).json({ success: true, favorited: result });
+  });
+  //그 다음에 프론트에 숫자 정보 보내주기
+});
+
 module.exports = router;
